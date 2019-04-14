0c91bf35f753eee2a6bf92ad94833693ab5b4b0f
Igor Kling <klingiv@altarix.ru>
2019-04-14 22:24:32 +0400

    store scroll position

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { createGlobalStyle } from 'styled-components'
 import { observable } from 'mobx'
 import { observer, Provider } from 'mobx-react'
 
@@ -35,13 +35,53 @@ html {
   }
 `
 
+
+class Store {
+  @observable scrollTop = 0
+}
+
+@observer
+class ScrollableContainer extends Component {
+
+  scrollRef = React.createRef()
+
+  componentDidMount() {
+    this.scrollRef.current.scrollTop = this.props.store.scrollTop;
+  }
+
+  handleScroll = event => {
+    this.props.store.scrollTop = this.scrollRef.current.scrollTop
+  }
+
+
+  render() {
+    return (
+      <div ref={this.scrollRef} style={{ height: '500px', overflow: 'scroll' }} onScroll={this.handleScroll}>
+        <div style={{ height: '400px', backgroundColor: 'yellow' }}></div>
+        <div style={{ height: '400px', backgroundColor: 'magenta' }}></div>
+      </div>
+    )
+  }
+}
+
+const store = new Store
+
+
 @observer
 export default class App extends Component {
+
+  state = { checked: false }
+
+  handleCheck = event => {
+    this.setState((state, props) => ({ checked: !state.checked }))
+  }
+
   render() {
     return (
       <>
         <GlobalStyle />
-        <div>Hello World!</div>
+        <input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked} />
+        {this.state.checked ? (<div>HELLO</div>) : (<ScrollableContainer store={store} />)}
       </>
     )
   }
